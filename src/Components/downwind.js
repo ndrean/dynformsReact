ab0500571734212b745ff57626209feeae8fc96f
neven <nevendrean@yahoo.fr>
2020-06-11 09:56:02 +0200

    aded material ui

--- src/Components/downwind.js
+++ src/Components/downwind.js
@@ -1,26 +1,72 @@
+// https://itnext.io/how-to-build-a-dynamic-controlled-form-with-react-hooks-2019-b39840f75c4f
+
 import React, { useState } from "react";
 //import "./styles.css";
+//import { FormControl } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+import ShowTrip from "./showTrip";
+//import classes from "*.module.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 export default function Downwind() {
+  const classes = useStyles();
+
   const newKiter = { name: "", contact: "" };
+  const newTrip = { date: "", startPoint: "", endPoint: "" };
+
   const ironMan = {
-    date: new Date("2020-09-01"),
+    date: new Date("2020-09-01T10:00"),
     startPoint: "Pipa",
     endPoint: "Sao Luis",
   };
-  //const newTrip = { date: "", startPoint: "", endPoint: "" };
+
   const [kiters, setKiters] = useState([newKiter]);
-  const [trip, setTrip] = useState(ironMan);
+  const [trip, setTrip] = useState(newTrip);
 
   function handleAddKiter() {
     setKiters([...kiters, { ...newKiter }]);
   }
 
   function handleSubmit(e) {
+    console.log(e.target.name);
     e.preventDefault();
     setTrip({ ...trip });
     setKiters([...kiters]);
     console.log(trip, kiters);
+  }
+
+  function handleChange(e) {
+    setTrip({ ...trip, [e.target.name]: e.target.value });
+  }
+
+  function NewInput(props) {
+    return (
+      <>
+        <label htmlFor={props.attr}>{props.children} </label>
+        <input
+          id={props.attr}
+          name={props.attr}
+          type={props.type}
+          value={[props.value]}
+          required={props.required}
+          onChange={props.onHandleChange}
+        />
+      </>
+    );
   }
 
   function handleRemoveKiter(index) {
@@ -36,84 +82,102 @@ export default function Downwind() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="date">New Downwind: </label>
-        <input
-          id="date"
-          name="date"
+      <form
+        onSubmit={handleSubmit}
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          required
+          id="standard-required0"
+          label="Choose a date"
+          variant="filled"
           type="date"
+          InputLabelProps={{
+            shrink: true,
+          }}
           value={trip.date}
           onChange={(e) => setTrip({ ...trip, date: e.target.value })}
         />
 
-        <label htmlFor="start">Start point:</label>
-        <input
-          type="text"
-          id="start"
-          name="start"
-          value={trip.startPoint}
+        <TextField
+          required
+          id="standard-required2"
+          label="Starting location"
+          variant="filled"
+          value={trip.start}
           onChange={(e) => setTrip({ ...trip, startPoint: e.target.value })}
         />
 
-        <label htmlFor="end">End point:</label>
-        <input
-          type="text"
-          id="end"
-          name="end"
-          value={trip.endPoint}
+        <TextField
+          required
+          id="standard-required2"
+          label="End Point"
+          variant="filled"
+          value={trip.end}
           onChange={(e) => setTrip({ ...trip, endPoint: e.target.value })}
         />
 
         {kiters.map((kiter, id) => {
+          // we iterate over the array to render
           const nameId = `name-${id}`;
           const contactId = `contact-${id}`;
+          // React asks for a key per div
           return (
             <div key={`kiter-${id}`}>
-              <label htmlFor={nameId}>{`Kiter ${id + 1}`}</label>
-              <input
-                type="text"
-                name={nameId}
-                id={nameId}
-                className="name"
+              <TextField
+                id="outlined-basic1"
+                label="Participant name"
+                variant="outlined"
                 value={kiter.name}
                 onChange={(e) => {
+                  // overide the spread array at the index 'id'
                   [...kiters][id].name = e.target.value;
                   return setKiters([...kiters]);
                 }}
-              />
-
-              <label htmlFor={contactId}>{`Contact ${id + 1}`}</label>
-              <input
-                type="text"
-                name={contactId}
-                id={contactId}
-                className="age"
+              >{`Kiter ${id + 1}`}</TextField>
+              <TextField
+                id="outlined-basic2"
+                label="Contact phone"
+                variant="outlined"
                 value={kiter.contact}
                 onChange={(e) => {
+                  // overide the spread array at the index 'id'
                   [...kiters][id].contact = e.target.value;
                   return setKiters([...kiters]);
                 }}
-              />
+              ></TextField>
             </div>
           );
         })}
-        <input
-          type="button"
-          value="Add new participant"
+        <br />
+        <Button
+          variant="outlined"
+          color="primary"
+          size="small"
           onClick={handleAddKiter}
-        />
+        >
+          Add new participant
+        </Button>
+
         <hr />
-        <input type="submit" value="Submit!" onSubmit={handleSubmit} />
+        <Button
+          type="submit"
+          onSubmit={handleSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+        >
+          Submit!
+        </Button>
       </form>
+      {kiters.length + 1 > 0 && trip.date !== "" && (
+        <p>Downwind scheduled for: {trip.date}</p>
+      )}
+
       <div>
-        <ul>
-          {kiters.map((kiter, id) => (
-            <li key={id}>
-              {kiter.name}
-              <button onClick={() => handleRemoveKiter(id)}>Del</button>
-            </li>
-          ))}
-        </ul>
+        <ShowTrip participants={kiters} onKiterRemove={handleRemoveKiter} />
       </div>
     </>
   );
---
+++ src/Components/showTrip.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Icon from "@material-ui/core/Icon";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth: 350,
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function ShowTrip(props) {
+  const classes = useStyles();
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Participant</TableCell>
+              <TableCell align="center">Phone</TableCell>
+              <TableCell align="left">Remove</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.participants.map((kiter, id) => (
+              <TableRow key={id}>
+                <TableCell component="th" scope="row">
+                  {kiter.name}
+                </TableCell>
+                <TableCell align="center">{kiter.contact}</TableCell>
+                <TableCell align="left">
+                  <IconButton
+                    aria-label="delete"
+                    color="secondary"
+                    onClick={() => props.onKiterRemove(id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
